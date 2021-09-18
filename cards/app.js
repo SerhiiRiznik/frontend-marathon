e90a0e9430072dcbf20ea8970f66fa031533a06d
SerhiiRiznik <69801777+SerhiiRiznik@users.noreply.github.com>
2021-09-18 20:50:24 +0200

    dragndrop

--- cards/app.js
+++ cards/app.js
@@ -2,15 +2,23 @@ const slides = document.querySelectorAll('.slide')
 
 
 
-for (const slide of slides) {
+slides.forEach((slide,index,array)=>{
    
-   slide.addEventListener('click',()=>{
-      clearActiveClasses()
-      slide.classList.add('active')
+   slide.addEventListener('click',(e)=>{
+      slide.classList.remove('active')
+         // slide.classList.remove('active')
+      
+      if (slide.classList.contains('slide')) {
+         slide.classList.toggle('active')
+      }
+      
        
    })
    
-}
+})
+   
+  
+
 
 function clearActiveClasses() {
    slides.forEach((slide)=>{
